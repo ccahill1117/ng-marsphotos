b04eb43c3e4520be3b9cb0728fe08b8ab09c982a
Chris Rudnicky and Robert Lee <chris@>
2019-01-29 10:09:49 -0800

    Add album id dynamic route.

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Album } from '../album.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-marketplace',
@@ -8,6 +9,8 @@ import { Album } from '../album.model';
 })
 
 export class MarketplaceComponent {
+constructor(private router: Router) {}
+
 albums: Album [] = [
   new Album("Pulse", "Pink Floyd", "A live album by the English prog rock band originally released in 1995, on the label EMI in the United Kingdom.",1),
   new Album("Funhouse", "The Stooges",
@@ -21,4 +24,8 @@ albums: Album [] = [
    new Album("Axis Bold As Love", "The Jimi Hendrix Experience",
        "Second studio album by the English-American band, released in 1967.", 6)
 ];
+
+goToDetailPage(clickedAlbum: Album) {
+  this.router.navigate(['albums', clickedAlbum.id]);
+};
 }
