c6e9d020d278d92ef7f7b2b65032ca8e1fbcd4f6
Chris Rudnicky and Robert Lee <robert@>
2019-01-29 09:54:43 -0800

    Add album-detail component, bootstrap, and a marketplace route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { MarketplaceComponent } from './marketplace/marketplace.component';
+import { AlbumsComponent } from './albums/albums.component';
+import { AboutComponent } from './about/about.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { routing } from './app.routing';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { AlbumDetailComponent } from './album-detail/album-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MarketplaceComponent,
+    AlbumsComponent,
+    AboutComponent,
+    WelcomeComponent,
+    AlbumDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    routing
   ],
   providers: [],
   bootstrap: [AppComponent]
